91698b5a89990d5b6d9a3c6d3641231516542328
Mark4546 <130235333+Mark4353@users.noreply.github.com>
2024-07-14 13:36:26 +0300

    js start

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+// Суворий режим
+"use strict"
+const myName = "Mark";
+console.log(myName);
+
+const sum = 2 + 9;
+console.log(sum);
+
+// myName = "Marik"
+// не можна змінити значеня тому що ми створили через const
+// щоб змінювати значеня треба писати let
+
+let myAge = "99";
+console.log(myAge);
+ 
+let myCity = "qwewer";
+console.log(myCity);
+
+
+let myCountry = "Ukraine";
+console.log(myCountry);
