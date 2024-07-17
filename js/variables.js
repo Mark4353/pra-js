f8d250330be3c794b7bc3250cd0c2e51ce13667e
Mark4546 <130235333+Mark4353@users.noreply.github.com>
2024-07-17 12:22:46 +0300

    hw 1 js

--- js/variables.js
+++ js/variables.js
@@ -1,21 +1,50 @@
 // Суворий режим
 "use strict"
-const myName = "Mark";
-console.log(myName);
 
-const sum = 2 + 9;
+
+const sum = 1 + 9;
 console.log(sum);
 
 // myName = "Marik"
 // не можна змінити значеня тому що ми створили через const
 // щоб змінювати значеня треба писати let
 
-let myAge = "99";
+let myAge = "0";
 console.log(myAge);
  
-let myCity = "qwewer";
+let myCity = "Zaporozhye";
 console.log(myCity);
-
 
 let myCountry = "Ukraine";
 console.log(myCountry);
+
+const isStudent = "false";
+console.log(isStudent);
+
+let myString = "Тут могла б бути ваша реклама";
+console.log(myString);
+
+const myName = "Mark";
+console.log("myName is");
+
+let myNull = "null";
+console.log(myNull);
+
+const confirmation = alert("eror 666")
+
+
+const accept = confirm("Підтвердіть дію!")
+console.log(accept)
+
+
+const notComing = confirm("дія відмінена!. Невідома помілка. reconect");
+console.log("notComing", false);
+
+const youName = prompt("введіть імя користувача");
+console.log(youName);
+
+const chek = prompt("На скількі ви балів від 0 до 100 вам сподобалась робота '(100)'?");
+console.log(chek);
+
+const isComing = confirm("Дякую за підтвердження!");
+console.log("isComing",);
