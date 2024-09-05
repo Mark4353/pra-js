c972234e0538479aacf672235fa30d82c72f41f8
Mark4546 <130235333+Mark4353@users.noreply.github.com>
2024-09-05 17:34:44 +0300

    hw-7

---
+++ js/hw-7.js
@@ -0,0 +1,56 @@
+// -----1----
+
+const numbers =['1','2','3'];
+
+numbers[1] ='10';
+console.log(numbers);
+
+// ----2----
+
+// const numbers =['1','2','3'];
+
+// numbers[3] = '4';
+
+// console.log(numbers);
+
+// ----3----
+
+// const numbers =['1','2','3'];
+// for(let i = 0; i < numbers.length; i += 1){
+//     console.log('Число -', numbers[i]);
+// }
+
+// ----4----
+
+// const numbers =['1','2','3','4','5'];
+// for(let i = 0; i < numbers.length; i += 1){
+//     console.log(numbers[i]);
+// }
+
+// ----5----
+
+// const numbers =['Жовтий','Три','Червоний','сік','Зелений'];
+
+// for(let i = 0 ; i < numbers.length; i += 2){
+// console.log(numbers[i]);
+// }
+
+// ----6----
+
+// const numbers =['1','2','3','4','5','6','7','8','9','10'];
+
+// console.log(numbers[9]);
+
+// ----7----
+
+// const numbers =['45','2','13','4','89','6','97','8','24','10'];
+
+// for(number of numbers){
+
+// if(number % 2 === 0)
+// console.log(number)
+// }
+
+
+
+
