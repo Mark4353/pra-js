61e7af2d02ba67c146877cba9a8fdd2244f9dfd6
Mark_Web <130235333+Mark4353@users.noreply.github.com>
2024-11-12 16:00:20 +0200

    hw-13

--- js/array-methods.js
+++ js/array-methods.js
@@ -10,4 +10,4 @@ console.log(`друг ${friend} є ${index} y ${arr}`)
     // Метод mad
 
 const friendList = friends.map((friend,index)=>`${friend } це друг, номер ${ index}`)
-console.log(friendList)
+console.log(friendList);
---
+++ js/hw-13.js
@@ -0,0 +1,100 @@
+console.log('--1--');
+const user = {
+  name: "Mark",
+  age: 24,
+  hobby: "ride-a-bike",
+  premium: true
+};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+
+for (const [key, value] of Object.entries(user)) {
+  console.log(`${key}: ${value}`);
+}
+
+console.log('--2--');
+
+function countProps(obj) {
+  const { length } = Object.keys(obj); 
+  return length;
+}
+console.log(countProps(user));
+
+console.log('--3--');
+
+const employees = {
+  Tim: 7,
+  Rob: 10,
+  Bob: 12
+};
+function findBestEmployee(employees) {
+  let maxTasks = 0;
+  let bestEmployee = "";
+
+  for (const [employee, tasks] of Object.entries(employees)) { 
+    if (tasks > maxTasks) {
+      maxTasks = tasks;
+      bestEmployee = employee;
+    }
+  }
+  return bestEmployee;
+}
+console.log(findBestEmployee(employees));
+
+console.log('--4--');
+
+function countTotalSalary(employees) {
+  let totalSalary = 0;
+
+  for (const salary of Object.values(employees)) {
+    totalSalary += salary;
+  }
+
+  return totalSalary;
+}
+console.log(countTotalSalary(employees));
+
+console.log('--5--');
+
+const products = [
+  { name: "Laptop", price: 1000, quantity: 5 },
+  { name: "Phone", price: 500, quantity: 10 },
+  { name: "Tablet", price: 800 }
+];
+
+function getAllPropValues(arr, prop) {
+  const values = [];
+  for (const product of arr) {
+    const { [prop]: value } = product; 
+    if (value !== undefined) {
+      values.push(value);
+    }
+  }
+  return values;
+}
+
+console.log(getAllPropValues(products, "name"));
+console.log(getAllPropValues(products, "quantity"));
+console.log(getAllPropValues(products, "price"));
+
+console.log('--6--');
+
+const products2 = [
+  { name: "Laptop", price: 1000, quantity: 5 },
+  { name: "Phone", price: 500, quantity: 10 },
+  { name: "Tablet", price: 800, quantity: 3 }
+];
+
+function calculateTotalPrice(allProducts, productName) {
+  for (const { name, price, quantity } of allProducts) { 
+    if (name === productName) {
+      return price * quantity;
+    }
+  }
+  return 0;
+}
+console.log(calculateTotalPrice(products2, "Laptop"));
+console.log(calculateTotalPrice(products2, "Phone"));
+console.log(calculateTotalPrice(products2, "Tablet"));
