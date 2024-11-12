0caf26d7723b5124b0d30291fadb0b62287c325c
Mark_Web <130235333+Mark4353@users.noreply.github.com>
2024-11-12 15:58:51 +0200

    hw-13

---
+++ js/array-methods.js
@@ -0,0 +1,13 @@
+// Назва-масиву,Назва-методу(()=>{})
+
+    const friends = ["Boba","Sara","Alex"];
+
+    // метод "forEach" заміняе "for"
+    friends.forEach((friend, index, arr)=>{
+console.log(`друг ${friend} є ${index} y ${arr}`)
+    });
+
+    // Метод mad
+
+const friendList = friends.map((friend,index)=>`${friend } це друг, номер ${ index}`)
+console.log(friendList)
