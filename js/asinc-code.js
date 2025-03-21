6549dec41fa4ab456fe8bc9115d50dd6e436fd66
der1x <130235333+Mark4353@users.noreply.github.com>
2025-03-21 18:10:37 +0200

    hw-10

---
+++ js/asinc-code.js
@@ -0,0 +1,17 @@
+console.log("test 1");
+console.log("test 2");
+
+setTimeout(() => {
+  console.log("test 3");
+}, 5000);
+
+console.log("test 4");
+
+setTimeout(() => {
+  console.log("test 5");
+}, 2000);
+
+setInterval(()=>{
+    console.log("test");
+},10)
+
---
+++ js/webstorage.js
@@ -0,0 +1,11 @@
+const userData = {
+    name: "user",
+    email: "user@mail.com",
+    pasword: "12345679@qwe",
+    list: ["apple", "banana", "strawberry"]
+ };
+
+console.log(userData);
+
+const userArxiv = JSON.stringify(userData);
+console.log(userArxiv);
